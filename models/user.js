a10f1ac04d25b0c87469f74026f645bfc630b54d
George Croucamp <george@bibliotech.education>
2019-01-22 14:59:46 +0000

    remove password field

--- models/user.js
+++ models/user.js
@@ -7,15 +7,6 @@ module.exports = function(sequelize, DataTypes, unusedParam, config) {
       allowNull: false,
       defaultValue: '',
     },
-    password: {
-      type: DataTypes.STRING,
-      set:  function(password) {
-        var salt = bcrypt.genSaltSync(10);
-        var hash = bcrypt.hashSync(password, salt);
-        if (password.length < 7) throw new Error('Please choose a longer password');
-        this.setDataValue('password', hash);
-      },
-    },
     email: {
       type: DataTypes.STRING,
       unique: true,
